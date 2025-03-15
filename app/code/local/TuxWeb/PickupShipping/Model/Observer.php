608ee12b727b438d3c76859034cbcadb44464757
Stefano <webmaster@logicagiochi.com>
2025-03-15 18:10:11 +0100

    add debug sistemo array

--- app/code/local/TuxWeb/PickupShipping/Model/Observer.php
+++ app/code/local/TuxWeb/PickupShipping/Model/Observer.php
@@ -47,13 +47,14 @@ class TuxWeb_PickupShipping_Model_Observer
                     $_title = $_code;
                 $options[] = array('value' => $_code, 'label' => $_title . " ($_code)");
 
-                Mage::helper('tuxweb_pickupshipping')->log('Lista Metodi: ' . print_r($options, true) );
                 /*if (strpos($rate->getCode(), 'owebiashipping1') !== false) {
                     $rates->removeItemByKey($rate->getId());
                     Mage::helper('tuxweb_pickupshipping')->log('Sto filtrando i metodi di spedizione. Metodo di spedizione nascosto: ' . $rate->getCode());
 
                 }*/
             }
+            Mage::helper('tuxweb_pickupshipping')->log('Lista Metodi: ' .$options[0]['value']." - " .$options[0]['label']." - ". print_r($options, true));
+
         }       
 
     }
